6e1e1a2bb1cd9e52db927bd7eb79637a264bab02
Natalya Myunster <natalyamyunster@yahoo.com>
2020-09-10 22:06:58 +0200

    added mongoose: npm i --save mongoose

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://admin:'+process.env.MONGO_ATLAS_PW + 'node-shop@node-rest-shop.3af3f.mongodb.net/<dbname>?retryWrites=true&w=majority')
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
