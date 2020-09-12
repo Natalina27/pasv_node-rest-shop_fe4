f2e6598e2edaceaf8c7aeafc72830e963649be8e
Natalya Myunster <natalyamyunster@yahoo.com>
2020-09-12 17:47:20 +0200

    Validation mongoose 1

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,16 +6,30 @@ const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   Product.find()
+      .select("name price _id")
       .exec()
       .then(docs => {
-        console.log(docs);
-          if (docs.length >= 0) {
-        res.status(200).json(docs);
-          } else {
-              res.status(404).json({
-                  message: 'No entries found'
-              });
-          }
+        const response = {
+          count: docs.length,
+          products: docs.map(doc => {
+            return{
+              name: doc.name,
+              price: doc.price,
+              _id: doc._id,
+              request:{
+                type: 'GET',
+                url: 'http://localhost:3000/products/' + doc._id
+              }
+            }
+          })
+        };
+            // if (docs.length >= 0) {
+            res.status(200).json(response);
+        // } else {
+        //     res.status(404).json({
+        //         message: 'No entries found'
+        //     });
+        // }
       })
       .catch(err => {
         console.log(err);
@@ -36,8 +50,16 @@ router.post("/", (req, res, next) => {
       .then(result => {
         console.log(result);
         res.status(201).json({
-          message: "Handling POST requests to /products",
-          createdProduct: result
+          message: "Created product successfully",
+          createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/products/' + result._id
+            }
+          }
         });
       })
       .catch(err => {
@@ -51,20 +73,27 @@ router.post("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+      .select('name price _id')
       .exec()
       .then(doc => {
         console.log("From database", doc);
         if (doc) {
-          res.status(200).json(doc);
+          res.status(200).json({
+            product: doc,
+            request: {
+              type: 'GET',
+              url: 'http://localhost/products'
+            }
+          });
         } else {
           res
               .status(404)
-              .json({ message: "No valid entry found for provided ID" });
+              .json({message: "No valid entry found for provided ID"});
         }
       })
       .catch(err => {
         console.log(err);
-        res.status(500).json({ error: err });
+        res.status(500).json({error: err});
       });
 });
 
@@ -74,7 +103,7 @@ router.patch("/:productId", (req, res, next) => {
   for (const ops of req.body) {
     updateOps[ops.propName] = ops.value;
   }
-  Product.update({ _id: id }, { $set: updateOps })
+  Product.update({_id: id}, {$set: updateOps})
       .exec()
       .then(result => {
         console.log(result);
@@ -90,7 +119,7 @@ router.patch("/:productId", (req, res, next) => {
 
 router.delete("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  Product.remove({ _id: id })
+  Product.remove({_id: id})
       .exec()
       .then(result => {
         res.status(200).json(result);
