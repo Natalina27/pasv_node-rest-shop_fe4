6e70ccd0bb3e3bfecdfca2620dda585fa094748a
Natalya Myunster <natalyamyunster@yahoo.com>
2020-09-18 20:25:37 +0200

    3. Handling error

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -12,5 +12,17 @@ router.post("/", (req, res) => {
     message: 'Order was created'
   });
 });
+router.get('/:orderId', (req, res) => {
+  res.status(200).json({
+    message: 'Order details',
+    orderId: req.params.orderId
+  });
+});
 
+router.delete('/:orderId', (req, res) => {
+  res.status(200).json({
+    message: 'Order deleted',
+    orderId: req.params.orderId
+  });
+});
 module.exports = router;
